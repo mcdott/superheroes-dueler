744103c562a6f593f61d8ce4c7dc9b1626e35908
mcdott <111994803+mcdott@users.noreply.github.com>
2022-11-04 14:28:32 -0400

    added attack method to Hero class

--- hero.py
+++ hero.py
@@ -1,13 +1,25 @@
 import random
+from ability import Ability
+from armour import Armour 
 
 class Hero:
   def __init__(self, name, starting_health=100):
+    self.abilities = list()
+    self.armour = list()
     self.name = name
     self.starting_health = starting_health
     self.current_health = starting_health
 
-  def add_ability(ability):
-    pass
+  def add_ability(self, ability):
+    self.abilities.append(ability)
+
+  def attack(self):
+    total_damage = 0
+    for ability in self.abilities:
+      total_damage += ability.attack()
+    return total_damage
+      
+
 
     
   def fight(self, opponent):
@@ -27,7 +39,14 @@ if __name__ == "__main__":
     # print(my_hero.name)
     # print(my_hero.current_health)
 
-    hero1 = Hero("Wonder Woman")
-    hero2 = Hero("Dumbledore")
+    # hero1 = Hero("Wonder Woman")
+    # hero2 = Hero("Dumbledore")
 
-    hero1.fight(hero2)
+    # hero1.fight(hero2)
+
+    ability = Ability("Great Debuggin", 50)
+    ability2 = Ability("Intelligence", 90)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    hero.add_ability(ability2)
+    print(hero.attack())
