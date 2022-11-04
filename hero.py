f665350c6ed69dd1042af739cd6d8785a3f752a3
mcdott <111994803+mcdott@users.noreply.github.com>
2022-11-04 17:44:35 -0400

    added stretch challenge to fight method

--- hero.py
+++ hero.py
@@ -41,66 +41,33 @@ class Hero:
     else:
       return False
 
-    
   def fight(self, opponent):
     if len(self.abilities) == 0 and len(opponent.abilities) == 0:
       print("Draw")
     else:
       while self.current_health > 0 and opponent.current_health > 0:
+        # Each fighter attacks and each fighter takes damage
         damage_to_opponent = self.attack()
         opponent.take_damage(damage_to_opponent)
-        if opponent.current_health <= 0:
-          print(f'{self.name} won!')
         damage_to_self = opponent.attack()
         self.take_damage(damage_to_self)
-        if self.current_health <= 0:
-          print(f'{opponent.name} won!')
 
-    # Chapter2:
-    # random_num = random.randint(0, 99)
-    # opponents_chance_of_winning_percentage = opponent.current_health/(opponent.current_health + self.current_health) * 100
-    # If the random number is less than the opponents chance of winning, the opponent wins
-    # if random_num < opponents_chance_of_winning_percentage:
-    #   print(f"{opponent.name} defeats {self.name}!")
-    # else:
-    #   print(f"{self.name} defeats {opponent.name}!")
+        # If at least one player has died, print the outcome of the fight
+        if self.current_health <= 0 and opponent.current_health <= 0:
+          print("Draw")
+        elif opponent.current_health <= 0:
+          print(f'{self.name} won!')
+        elif self.current_health <= 0:
+          print(f'{opponent.name} won!')
 
 
 if __name__ == "__main__":
-    
-    # my_hero = Hero("Grace Hopper", 200)
-    # print(my_hero.name)
-    # print(my_hero.current_health)
-
-    # hero1 = Hero("Wonder Woman")
-    # hero2 = Hero("Dumbledore")
-
-    # hero1.fight(hero2)
-
-    # debugging = Ability("Great Debuggin", 50)
-    # intelligence = Ability("Intelligence", 90)
-    # hero = Hero("Grace Hopper", 200)
-    # hero.add_ability(debugging)
-    # hero.add_ability(intelligence)
-    # print(f'Attack: {hero.attack()}')
-
-    # persistence = Armour("Persistence", 75)
-    # self_confidence = Armour("Self-confidence", 50)
-    # hero.add_armour(persistence)
-    # hero.add_armour(self_confidence)
-    # print(f'Defence: {hero.defend()}')
-
-    # hero = Hero("Grace Hopper", 200)
-    # hero.take_damage(150)
-    # print(hero.is_alive())
-    # hero.take_damage(15000)
-    # print(hero.is_alive())
 
     hero1 = Hero("Wonder Woman")
     hero2 = Hero("Dumbledore")
     ability1 = Ability("Super Speed", 300)
     ability2 = Ability("Super Eyes", 130)
-    ability3 = Ability("Wizard Wand", 80)
+    ability3 = Ability("Wizard Wand", 180)
     ability4 = Ability("Wizard Beard", 20)
     hero1.add_ability(ability1)
     hero1.add_ability(ability2)
