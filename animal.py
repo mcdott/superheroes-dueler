77e2bfa20505f65afceda2c9f705ae0692e50d4c
mcdott <111994803+mcdott@users.noreply.github.com>
2022-11-04 17:56:56 -0400

    added the Animal and Frog classes to practice inheritance

---
+++ animal.py
@@ -0,0 +1,18 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self):
+        print(f'{self.name} is eating')
+
+    def drink(self):
+        print(f'{self.name} is drinking')
+
+class Frog(Animal):
+    def jump(self):
+        print(f'{self.name} is jumping')
+
+freddy = Frog("Freddy")
+freddy.eat()
+freddy.drink()
+freddy.jump()
