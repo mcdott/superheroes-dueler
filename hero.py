82bb35d3105edf9fa3f6d8413873c21c317f6532
mcdott <111994803+mcdott@users.noreply.github.com>
2022-11-04 14:47:08 -0400

    added defend method to Hero class

--- hero.py
+++ hero.py
@@ -18,7 +18,17 @@ class Hero:
     for ability in self.abilities:
       total_damage += ability.attack()
     return total_damage
-      
+
+  def add_armour(self, armour):
+    self.armour.append(armour)
+
+  def defend(self):
+    total_block = 0
+    if len(self.armour) > 0 and self.current_health > 0:  
+      for armour in self.armour:
+        total_block += armour.block()
+    return total_block
+
 
 
     
@@ -44,9 +54,15 @@ if __name__ == "__main__":
 
     # hero1.fight(hero2)
 
-    ability = Ability("Great Debuggin", 50)
-    ability2 = Ability("Intelligence", 90)
+    debugging = Ability("Great Debuggin", 50)
+    intelligence = Ability("Intelligence", 90)
     hero = Hero("Grace Hopper", 200)
-    hero.add_ability(ability)
-    hero.add_ability(ability2)
-    print(hero.attack())
+    hero.add_ability(debugging)
+    hero.add_ability(intelligence)
+    print(f'Attack: {hero.attack()}')
+
+    persistence = Armour("Persistence", 75)
+    self_confidence = Armour("Self-confidence", 50)
+    hero.add_armour(persistence)
+    hero.add_armour(self_confidence)
+    print(f'Defence: {hero.defend()}')
