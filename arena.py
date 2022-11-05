3858d800d39a04594c0e99978b5b7139f9f7a8c7
mcdott <111994803+mcdott@users.noreply.github.com>
2022-11-05 15:50:44 -0400

    Fixed issues with Arean class and fixed spelling in arena.py file name

--- arena.py
+++ arena.py
@@ -10,15 +10,15 @@ class Arena:
         team_one: None
         team_two: None
     '''
-    self.team_one = list()
-    self.team_two = list()
+    self.team_one = Team('Team One')
+    self.team_two = Team('Team Two')
 
   def create_ability(self):
     '''Prompt for Ability information.
       return Ability with values from user Input
     '''
     name = input("What is the ability name?: ")
-    max_damage = input("What is the max damage of the ability?: ")
+    max_damage = int(input("What is the max damage of the ability?: "))
 
     return Ability(name, max_damage)
 
@@ -27,7 +27,7 @@ class Arena:
         return Weapon with values from user input.
     '''
     name = input("What is the weapon name?: ")
-    max_damage = input("What is the max damage of the weapon?: ")
+    max_damage = int(input("What is the max damage of the weapon?: "))
 
     return Weapon (name, max_damage)
 
@@ -36,7 +36,7 @@ class Arena:
       return Armour with values from user input.
     '''
     name = input("What is the name of the piece of armour?: ")
-    max_block = input("What is the max block of the piece of armour?: ")
+    max_block = int(input("What is the max block of the piece of armour?: "))
 
     return Armour (name, max_block)
 
@@ -66,22 +66,18 @@ class Arena:
     num_of_team_members = int(input("How many members would you like on Team One?: \n"))
     for i in range(num_of_team_members):
       hero = self.create_hero()
-      self.team_one.append(hero)
+      self.team_one.add_hero(hero)
 
   def build_team_two(self):
     '''Prompt the user to build team_two'''
     num_of_team_members = int(input("How many members would you like on Team Two?: \n"))
     for i in range(num_of_team_members):
       hero = self.create_hero()
-      self.team_two.append(hero)
+      self.team_two.add_hero(hero)
 
   def team_battle(self):
     '''Battle team_one and team_two together.'''
-    # TODO: This method should battle the teams together.
-    # Call the attack method that exists in your team objects
-    # for that battle functionality.
-    # ...try instantiating Team objects
-    self.team_one[1].attack(self.team_two[1])
+    self.team_one.attack(self.team_two)
 
   def show_stats(self):
     '''Prints team statistics to terminal.'''
--- team.py
+++ team.py
@@ -32,6 +32,8 @@ class Team:
     def stats(self):
         '''Print team statistics'''
         for hero in self.heroes:
+            if hero.deaths == 0:
+                hero.deaths = 1
             kd = hero.kills / hero.deaths
             print(f"{hero.name} Kill/Deaths:{kd}")
 
